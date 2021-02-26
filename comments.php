329f5e7915c41873474574545f1864655e20f642
Tim Loden <tim.m.loden@gmail.com>
2021-02-26 11:37:24 -0800

    updated @package to starter-theme

--- comments.php
+++ comments.php
@@ -7,7 +7,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package underscores
+ * @package starter-theme
  */
 
 /*
@@ -22,12 +22,12 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
-	<?php
+    <?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
-			<?php
+    <h2 class="comments-title">
+        <?php
 			$underscores_comment_count = get_comments_number();
 			if ( '1' === $underscores_comment_count ) {
 				printf(
@@ -44,27 +44,27 @@ if ( post_password_required() ) {
 				);
 			}
 			?>
-		</h2><!-- .comments-title -->
+    </h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
+    <?php the_comments_navigation(); ?>
 
-		<ol class="comment-list">
-			<?php
+    <ol class="comment-list">
+        <?php
 			wp_list_comments( array(
 				'style'      => 'ol',
 				'short_ping' => true,
 			) );
 			?>
-		</ol><!-- .comment-list -->
+    </ol><!-- .comment-list -->
 
-		<?php
+    <?php
 		the_comments_navigation();
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'underscores' ); ?></p>
-			<?php
+    <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'underscores' ); ?></p>
+    <?php
 		endif;
 
 	endif; // Check for have_comments().
